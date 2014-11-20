880d91f2f94b2c26028c2f81916fcf1eaed44fe5
Michael Bischoff <jpaw@online.de>
2014-11-20 18:40:44 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 GOAL=install
-if [ x != x$2 ]; then
-	GOAL=$2
+if [ x != x$1 ]; then
+	GOAL=$1
 fi
 
+echo "Cleaning repo folder..."
 git clean -fdx
+
+echo "executing mvn $GOAL..."
 (cd jpaw-defs && mvn $GOAL)
 (cd jpaw-base && mvn $GOAL)

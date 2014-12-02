d4a5e478608e26fcaaa9f4f5642121728e246652
Michael Bischoff <jpaw@online.de>
2014-12-02 22:24:30 +0100

    set of enums via active annotation

---
+++ jpaw-enumsets-test/src/test/xtend/de/jpaw/enumsets/tests/SetOfEnumsTest.xtend
@@ -0,0 +1,23 @@
+package de.jpaw.enumsets.tests
+
+import de.jpaw.enumsets.AbstractEnumSet
+import de.jpaw.enumsets.SetOfEnum
+import org.testng.annotations.Test
+
+enum Weekday {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
+
+@SetOfEnum
+class SetOfWeekdays extends AbstractEnumSet<Weekday> {}
+
+class SetOfEnumsTest {
+
+    @Test
+    def public void testSetOfEnums() {
+        val mySet = SetOfWeekdays.of(Weekday.MONDAY, Weekday.WEDNESDAY, Weekday.FRIDAY)
+        
+        for (d : mySet)
+            println('''«d.name» is in the set''')
+    }    
+}
---
+++ jpaw-enumsets/src/main/java/de/jpaw/enumsets/AbstractEnumSet.java
@@ -0,0 +1,112 @@
+package de.jpaw.enumsets;
+
+import java.io.Serializable;
+import java.util.AbstractCollection;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.Consumer;
+
+/** An alternate implementation of EnumSet, but with the ability to obtain the resulting bitmap, for either transfer or storing in a database.
+ * The underlying object is an int, therefore the maximum number of enum tokens is 31 (as we don't want negative values). */
+public abstract class AbstractEnumSet<E extends Enum<E>> extends AbstractCollection<E> implements Set<E>, Serializable {
+    private static final long serialVersionUID = 3439839098917393638L;
+    private int bitmap;
+    
+    abstract protected int getMaxOrdinal();
+    
+    public AbstractEnumSet() {
+        bitmap = 0;
+    }
+    
+    public AbstractEnumSet(int bitmap) {
+        this.bitmap = bitmap;
+    }
+    
+    
+    public int getBitmap() {
+        return bitmap;
+    }
+    
+    @Override
+    public int size() {
+        return Integer.bitCount(bitmap);
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return bitmap == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null || !(o instanceof Enum))
+            return false;
+        int q = ((Enum<?>)o).ordinal();
+        return q < 32 && (bitmap & (1 << q)) != 0;
+    }
+    
+    @Override
+    public boolean add(E e) {
+        int q = e.ordinal();   // may throw NPE
+        if (q >= 31)
+            throw new IllegalArgumentException(e.getClass().getCanonicalName() + "." + e.name() + " has ordinal " + e.ordinal());
+        int b = 1 << q;
+        if ((bitmap & b) != 0)
+            return false;
+        bitmap |= b;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        E e = (E)o;
+        int q = e.ordinal();   // may throw NPE
+        if (q >= 31)
+            throw new IllegalArgumentException(e.getClass().getCanonicalName() + "." + e.name() + " has ordinal " + e.ordinal());
+        int b = 1 << q;
+        if ((bitmap & b) != 0)
+            return false;
+        bitmap &= ~b;
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        bitmap = 0;
+    }
+    
+    static protected class SetOfEnumsIterator<E extends Enum<E>> implements Iterator<E> {
+        private final Class<E> enumType;
+        private final String [] values;
+        private int bitmap;
+        private int index;
+        
+        protected SetOfEnumsIterator(Class<E> enumType, String [] values, int bitmap) {
+            this.enumType = enumType;
+            this.values = values;
+            this.bitmap = bitmap;
+            this.index = -1;
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return bitmap != 0;
+        }
+
+        @Override
+        public E next() {
+            if (bitmap == 0)
+                return null;
+            ++index;                                    // index was at previous position. At least increment once
+            while ((bitmap & (1 << index)) == 0)
+                ++index;
+            bitmap &= ~ (1 << index);
+            return Enum.valueOf(enumType, values[index]);
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}

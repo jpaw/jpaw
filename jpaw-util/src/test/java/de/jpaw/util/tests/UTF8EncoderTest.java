8a8b0e6457d723333e53782a3d1e26d273567baa
Michael Bischoff <jpaw@online.de>
2015-02-22 10:35:05 +0100

    add CESU encode, add string encoder / decoder benchmarks

---
+++ jpaw-util/src/test/java/de/jpaw/util/tests/UTF8EncoderTest.java
@@ -0,0 +1,40 @@
+package de.jpaw.util.tests;
+
+import java.io.UnsupportedEncodingException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.jpaw.util.Cesu8Encoder;
+
+public class UTF8EncoderTest {
+    
+    @Test
+    public void testEncoder() throws UnsupportedEncodingException {
+        // for the range tested, CESU8 is identical to UTF8
+        for (int i = 0; i < 0xd800; ++i) {
+            String testString = "A" + Character.valueOf((char)i) + "O";
+            byte [] fromString = testString.getBytes("UTF-8");
+            byte [] myOwn = Cesu8Encoder.encodeToCesu8(testString);
+            Assert.assertEquals(fromString, myOwn, "for index " + i);
+        }
+    }
+    
+//  commented out, CESU8 direct was experimental
+//    @Test
+//    public void testUnsafeEncoder() throws UnsupportedEncodingException {
+//        for (int i = 0; i < 0xd800; ++i) {
+//            String testString = "A" + Character.valueOf((char)i) + "O";
+//            byte [] fromString = testString.getBytes("UTF-8");
+//            byte [] myOwn = Cesu8DirectEncoder.encodeToCesu8(testString);
+//            Assert.assertEquals(fromString, myOwn, "for index " + i);
+//        }
+//    }
+//    @Test
+//    public void testEncoder1() throws UnsupportedEncodingException {
+//        String testString = "A" + Character.valueOf((char) 55296) + "O";
+//        byte[] fromString = testString.getBytes("UTF-8");
+//        byte[] myOwn = UTF8Encoder.encodeToCESU8(testString);
+//        Assert.assertEquals(fromString, myOwn);
+//    }
+}
---
+++ jpaw-util/src/test/java/de/jpaw/util/tests/UnsafeTest.java
@@ -0,0 +1,20 @@
+package de.jpaw.util.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.jpaw.util.UnsafeUtil;
+
+public class UnsafeTest {
+    @Test
+    public void testUnsafe() {
+        String testString = "JDK6";
+        
+        char [] array = UnsafeUtil.getStringBuffer(testString);
+        Assert.assertEquals(array.length, 4);
+        
+        array[3] = '7';
+        
+        System.out.println("I'm at least on " + testString);
+    }
+}

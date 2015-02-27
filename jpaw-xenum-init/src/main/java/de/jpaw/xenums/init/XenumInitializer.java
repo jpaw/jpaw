c9e21caf8ec29ac2812d5fbfc3fe4d2e9556ca0e
Michael Bischoff <jpaw@online.de>
2015-02-27 18:23:58 +0100

    add methods to pass arrays of prescanned Reflections

--- jpaw-xenum-init/src/main/java/de/jpaw/xenums/init/XenumInitializer.java
+++ jpaw-xenum-init/src/main/java/de/jpaw/xenums/init/XenumInitializer.java
@@ -1,5 +1,6 @@
 package de.jpaw.xenums.init;
 
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,15 +10,21 @@ public class XenumInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger(XenumInitializer.class);
     
     public static void initializeXenums(String packageName) {
-        int ctr = 0;
-        for (Class<? extends AbstractXEnumBase> cls : ReflectionsPackageCache.get(packageName).getSubTypesOf(AbstractXEnumBase.class)) {
-            try {
-                cls.getMethod("xenum$MetaData").invoke(null);
-                ++ctr;
-            } catch (Exception e) {
-                LOGGER.warn("Cannot initialize xenum {}: {}", cls.getCanonicalName(), e.getMessage());
+        initializeXenums(ReflectionsPackageCache.get(packageName));
+    }
+    
+    public static void initializeXenums(Reflections ... reflections) {
+        for (int i = 0; i < reflections.length; ++i) {
+            int ctr = 0;
+            for (Class<? extends AbstractXEnumBase> cls : reflections[i].getSubTypesOf(AbstractXEnumBase.class)) {
+                try {
+                    cls.getMethod("xenum$MetaData").invoke(null);
+                    ++ctr;
+                } catch (Exception e) {
+                    LOGGER.warn("Cannot initialize xenum {}: {}", cls.getCanonicalName(), e.getMessage());
+                }
             }
+            LOGGER.info("Startup: Loaded {} xenum classes", ctr);
         }
-        LOGGER.info("Startup: Loaded {} xenum classes", ctr);
     }
 }

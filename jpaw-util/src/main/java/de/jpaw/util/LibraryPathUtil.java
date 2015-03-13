a328d17abce2e1d9d7faa13d32ddab3670ee46d6
Michael Bischoff <jpaw@online.de>
2015-03-13 19:43:53 +0100

    add util to set java library path programmatically

---
+++ jpaw-util/src/main/java/de/jpaw/util/LibraryPathUtil.java
@@ -0,0 +1,32 @@
+package de.jpaw.util;
+
+import java.lang.reflect.Field;
+
+public class LibraryPathUtil {
+    
+    // workaround caching required. See http://blog.cedarsoft.com/2010/11/setting-java-library-path-programmatically/
+    public static void setSystemLibraryPath(String path) {
+        System.out.println("Setting library path to " + path);
+        System.setProperty( "java.library.path", path);
+    
+        Field fieldSysPath;
+        try {
+            fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+            fieldSysPath.setAccessible( true );
+            fieldSysPath.set( null, null );
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void setDefaultSystemLibraryPath() {
+        String path = System.getProperty("user.home") + "/lib";
+        setSystemLibraryPath(path);
+    }
+}

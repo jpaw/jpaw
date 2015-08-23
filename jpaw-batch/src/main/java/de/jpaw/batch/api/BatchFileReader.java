72a2d4ef1f841d04d3b2966a73fc2cef5a1beaa5
Michael Bischoff <jpaw@online.de>
2015-08-23 17:55:03 +0200

    provide additional interface for data sources

---
+++ jpaw-batch/src/main/java/de/jpaw/batch/api/BatchFileReader.java
@@ -0,0 +1,11 @@
+package de.jpaw.batch.api;
+
+
+/** API of file (or stream) based inputs */ 
+public interface BatchFileReader<E> extends BatchReader<E> {
+
+    String getFilename();       // provides the source filename
+    int getSkip();              // provides the number of lines to skip
+    int getMaxRecords();        // provides the maximum number of lines to process;
+    String getEncoding();       // provides the configured input file encoding;
+}

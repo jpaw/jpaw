21675eb48a3e6f4a4d20873b57d0485d9ee6d8e7
Michael Bischoff <jpaw@online.de>
2015-11-20 19:55:51 +0100

    new class to estimate best initial map size for given number of elements

---
+++ jpaw-util/src/main/java/de/jpaw/util/CollectionUtil.java
@@ -0,0 +1,12 @@
+package de.jpaw.util;
+
+public class CollectionUtil {
+    /** Pick a size for a map which avoids resizing for the default load factor (0.75).
+     * Also leave some gap...
+     * @param numEntries The number of expected entries
+     * @return the size which allows a map without resizing
+     */
+    public static int mapInitialSize(int numEntries) {
+        return numEntries <= 0 ? 0 : numEntries + ((1 + numEntries) >> 1);      // good for a load factor as low as 0.6667
+    }
+}

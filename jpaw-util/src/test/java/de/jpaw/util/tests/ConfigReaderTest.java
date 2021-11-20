e99191d9f3befdc7e63cf4d865bc422cec260d6a
Michael Bischoff <jpaw@online.de>
2021-11-20 17:02:54 +0100

    fix test

--- jpaw-util/src/test/java/de/jpaw/util/tests/ConfigReaderTest.java
+++ jpaw-util/src/test/java/de/jpaw/util/tests/ConfigReaderTest.java
@@ -1,5 +1,8 @@
 package de.jpaw.util.tests;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +13,13 @@ public class ConfigReaderTest {
 
     @Test
     public void testProperties() throws Exception {
-        final ConfigurationReader cfgReader = ConfigurationReaderFactory.getDefaultJpawConfigReader();
+        final Path resourceDirectory = Paths.get("src","test","resources");
+        final String absolutePath = resourceDirectory.toFile().getAbsolutePath() + "/jpaw.properties";
+        final ConfigurationReader cfgReader = ConfigurationReaderFactory.getConfigReader(absolutePath);
 
         Assertions.assertNull(cfgReader.getProperty("wedonothavethis"), "Did not expect a missing prperty to be non null");
         Assertions.assertEquals("string1", cfgReader.getProperty("jpaw.testvalue"), "string check");
-        Assertions.assertEquals(Boolean.TRUE, cfgReader.getProperty("testbool"), "boolean check");
-        Assertions.assertEquals(46, cfgReader.getProperty("jpaw.testnum"), "numeric check");
+        Assertions.assertEquals(Boolean.TRUE, cfgReader.getBooleanProperty("testbool"), "boolean check");
+        Assertions.assertEquals(46, cfgReader.getIntProperty("jpaw.testnum"), "numeric check");
     }
 }

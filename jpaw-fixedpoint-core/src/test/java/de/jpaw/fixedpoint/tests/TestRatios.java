0d65de47a231710130baee671eba9ba15e6a3fcc
Michael Bischoff <Michael.Bischoff@Bertelsmann.de>
2024-08-06 19:19:42 +0200

    JPAW-9: add slf4j-simple output for tests

--- jpaw-fixedpoint-core/src/test/java/de/jpaw/fixedpoint/tests/TestRatios.java
+++ jpaw-fixedpoint-core/src/test/java/de/jpaw/fixedpoint/tests/TestRatios.java
@@ -4,18 +4,24 @@ import java.math.RoundingMode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.jpaw.fixedpoint.types.MicroUnits;
 
 
 public class TestRatios {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestRatios.class);
+
     private static final MicroUnits START_VALUE = MicroUnits.of(15_777_333_234_567_321L);  // need something which definitely exceeds 32 bits
     private static final long EPSILON = 1L;  // we cannot compare to 1, because we have varying rounding modes
 
-//    @Test
-//    public void testRatioSimple() throws Exception {
-//        final MicroUnits result = START_VALUE.multAndDivide(777_666_333, 999_888_444, RoundingMode.HALF_EVEN);
-//    }
+    @Test
+    public void testRatioSimple() throws Exception {
+        // simple test for debugging / single stepping
+        final MicroUnits result = START_VALUE.multAndDivide(777_666_333, 999_888_444, RoundingMode.HALF_EVEN);
+        LOGGER.debug("result is {}", result);
+    }
 
     protected void testRatios(final MicroUnits startNumber, final int startNominator, final int startDenominator,
       final int scalePerIteration, final RoundingMode roundingMode) {
